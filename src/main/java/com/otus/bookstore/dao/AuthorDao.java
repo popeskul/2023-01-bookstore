9ef0008913559f28720e08e8974488c60b268694
Pavlo Popeskul <peterjj91@gmail.com>
2023-03-08 21:27:12 +0200

    hw9-bookshelf-spring-jdbc: refactored interface AuthorDao

--- src/main/java/com/otus/bookstore/dao/AuthorDao.java
+++ src/main/java/com/otus/bookstore/dao/AuthorDao.java
@@ -7,9 +7,14 @@ import java.util.Optional;
 
 public interface AuthorDao {
     Optional<Integer> insert(Author author);
+
     void update(Author author);
+
     void deleteById(int id);
+
     Optional<Author> getById(int id);
+
     List<Author> getAll();
+
     void deleteAll();
 }
