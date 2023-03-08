07cbaeca47c516c286f45813cc67949f589ef74d
Pavlo Popeskul <peterjj91@gmail.com>
2023-03-08 21:25:55 +0200

    hw9-bookshelf-spring-jdbc: removed opening console

--- src/main/java/com/otus/bookstore/Application.java
+++ src/main/java/com/otus/bookstore/Application.java
@@ -1,16 +1,13 @@
 package com.otus.bookstore;
 
-import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        Console.main(args);
     }
 
 }
