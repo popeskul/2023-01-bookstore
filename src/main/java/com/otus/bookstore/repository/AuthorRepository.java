bc6b1edc526b2b8d6f062e5aae1f126384dbc878
Pavlo Popeskul <peterjj91@gmail.com>
2023-04-06 20:26:23 +0300

    hw12-13/spring-data: refactored AuthorRepository with the JpaRepository<Author, Long>

--- src/main/java/com/otus/bookstore/repository/AuthorRepository.java
+++ src/main/java/com/otus/bookstore/repository/AuthorRepository.java
@@ -1,16 +1,7 @@
 package com.otus.bookstore.repository;
 
 import com.otus.bookstore.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface AuthorRepository {
-    Optional<Author> findById(long id);
-
-    List<Author> findAll();
-
-    Author save(Author author);
-
-    boolean deleteById(long id);
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
