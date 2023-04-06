b7b5dd2c4b2407718768ff2d813d5d2fa7554b80
Pavlo Popeskul <peterjj91@gmail.com>
2023-04-06 19:57:53 +0300

    hw12-13/spring-data: refactored GenreRepository with the JpaRepository<Genre, Long>

--- src/main/java/com/otus/bookstore/repository/GenreRepository.java
+++ src/main/java/com/otus/bookstore/repository/GenreRepository.java
@@ -1,16 +1,7 @@
 package com.otus.bookstore.repository;
 
 import com.otus.bookstore.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface GenreRepository {
-    Optional<Genre> findById(long id);
-
-    List<Genre> findAll();
-
-    Optional<Genre> save(Genre author);
-
-    void deleteById(long id);
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
